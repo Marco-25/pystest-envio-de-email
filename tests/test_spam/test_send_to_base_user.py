1d9b55708046e3613ad147fdeb8661c08f69c41c
Marco-25 <marco.antonio-25@outlook.com.br>
2021-07-19 17:54:45 -0300

    Utilizando unittest mock

--- tests/test_spam/test_send_to_base_user.py
+++ tests/test_spam/test_send_to_base_user.py
@@ -1,39 +1,9 @@
+from unittest.mock import Mock
+
 import pytest
-from typing import List
 
 from spam.main import SenderSpam
 from spam.models import User
-from spam.send_email import SendEmail
-
-
-class SendEmailMock(SendEmail):
-    """SendEmailMocke classe que simula a classe SendEmail
-    O conceito de mock é trocar um objecto por outro
-    """
-    def __init__(self) -> None:
-        super().__init__()
-        self.__parameters_send: List[str] = []
-        self.__qtd_email_sent: int = 0
-
-    @property
-    def parameters_send(self):
-        return self.__parameters_send
-
-    @parameters_send.setter
-    def parameters_send(self, params: List[str]) -> None:
-        self.__parameters_send = params
-
-    @property
-    def qtd_email_sent(self) -> int:
-        return self.__qtd_email_sent
-
-    @qtd_email_sent.setter
-    def qtd_email_sent(self, value: int) -> None:
-        self.__qtd_email_sent = value
-
-    def send(self, sender: str, to: str, subject_matter: str, body: str) -> None:
-        self.parameters_send = (sender, to, subject_matter, body)
-        self.qtd_email_sent += 1
 
 
 @pytest.mark.parametrize(
@@ -51,27 +21,27 @@ class SendEmailMock(SendEmail):
 def test_send_spam(session, users):
     for user in users:
         session.save(user)
-    email = SendEmailMock()
+    email = Mock()
     sender_spam = SenderSpam(session, email)
     sender_spam.send_email(
         'remetente@teste.com',
         'assunto do email',
         'corpo do email (mensagem do email)'
     )
-    assert len(users) == email.qtd_email_sent
+    assert len(users) == email.send.call_count
 
 
 def test_parameters_send_spam(session):
     user = User(name='Marco', email="marco@test.com")
     session.save(user)
-    email = SendEmailMock()
+    email = Mock()
     sender_spam = SenderSpam(session, email)
     sender_spam.send_email(
         'remetente_teste@teste.com',
         'assunto do email',
         'corpo do email (mensagem do email)'
     )
-    assert email.parameters_send == (
+    email.send.assert_called_once_with(
         'remetente_teste@teste.com',
         'marco@test.com',
         'assunto do email',
